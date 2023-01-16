d6e3cf9114077401fe99d0befd14187ad5d0c668
ji <vyoungv7340naver.com>
2023-01-16 10:50:59 +0900

    Fixed untracked files

--- src/views/Detail/Detail.vue
+++ src/views/Detail/Detail.vue
@@ -2,122 +2,138 @@
   <div class="section detail">
     <h3 class="section-tit">캠페인 상세</h3>
     <div class="top-info">
-      <h4 class="company">{{detailData[0].campaign_title}}</h4>
-      <p class="campaign">{{detailData[0].company_name}}</p>
-      <p class="period">{{detailData[0].start_day?.split('T')[0]}} ~ {{detailData[0].end_day?.split('T')[0]}}</p>
+      <h4 class="company">{{ detailData[0].campaign_title }}</h4>
+      <p class="campaign">{{ detailData[0].company_name }}</p>
+      <p class="period">
+        {{ detailData[0].start_day?.split("T")[0] }} ~
+        {{ detailData[0].end_day?.split("T")[0] }}
+      </p>
     </div>
     <div class="table-wrap">
       <div class="table-info">
         <div class="total-num">
-          총 <span>{{detailData[0].pictures.length}}</span>개
+          총 <span>{{ detailData[0].pictures.length }}</span
+          >개
         </div>
         <button type="button" class="btn-blue-sm">일괄 다운로드</button>
       </div>
       <table>
         <colgroup>
-          <col width="40px;"/>
-          <col width="200px"/>
-          <col width="200px;"/>
-          <col width="80px"/>
-          <col width=""/>
-          <col width="200px"/>
-          <col width="180px"/>
+          <col width="40px;" />
+          <col width="200px" />
+          <col width="200px;" />
+          <col width="80px" />
+          <col width="" />
+          <col width="200px" />
+          <col width="180px" />
         </colgroup>
         <thead>
-            <tr>
-                <th>No.</th>
-                <th>날짜</th>
-                <th>캡쳐시간</th>
-                <th>순위</th>
-                <th>인플루언서</th>
-                <th>키워드</th>
-                <th>이미지</th>
-            </tr>
+          <tr>
+            <th>No.</th>
+            <th>날짜</th>
+            <th>캡쳐시간</th>
+            <th>순위</th>
+            <th>인플루언서</th>
+            <th>키워드</th>
+            <th>이미지</th>
+          </tr>
         </thead>
         <tbody>
-            <tr v-for="(list,i) in detailData[0].pictures" :key="i">
-              <td>{{detailData[0].pictures.length - i}}</td>
-              <td>{{list.created_at?.split('T')[0]}}</td>
-              <td>{{list.created_at?.split('T')[1].split('\.')[0]}}</td>
-              <td>
-                <p v-for="(rank,i) in list.rank">{{rank.rank}}</p>
-              </td>
-              <td>
-                <p v-for="(rank,i) in list.rank"><a class="underline" :href="rank.url" target="_blank">Post로 이동</a></p>
-              </td>
-              <td>{{list.keyword}}</td>
-              <td>
-                <p v-for="(rank,i) in list.rank">
-                  <a target="_blank" :href="`http://3.36.103.222:80/scheduler/image?bucketPath=${list.bucket_path}`">보기</a>
-                  <button type="button">다운로드</button>
-                </p>
-              </td>
-            </tr>
+          <tr v-for="(list, i) in detailData[0].pictures" :key="i">
+            <td>{{ detailData[0].pictures.length - i }}</td>
+            <td>{{ list.created_at?.split("T")[0] }}</td>
+            <td>{{ list.created_at?.split("T")[1].split("\.")[0] }}</td>
+            <td>
+              <p v-for="(rank, i) in list.rank">{{ rank.rank }}</p>
+            </td>
+            <td>
+              <p v-for="(rank, i) in list.rank">
+                <a class="underline" :href="rank.url" target="_blank"
+                  >Post로 이동</a
+                >
+              </p>
+            </td>
+            <td>{{ list.keyword }}</td>
+            <td>
+              <p v-for="(rank, i) in list.rank">
+                <a
+                  target="_blank"
+                  :href="`http://3.36.103.222:80/scheduler/image?bucketPath=${list.bucket_path}`"
+                  >보기</a
+                >
+                <button type="button">다운로드</button>
+              </p>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
     <div class="t-right">
-      <button type="button" class="btn-blue-sm" @click="moveList">목록보기</button>
+      <button type="button" class="btn-blue-sm" @click="moveList">
+        목록보기
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  data(){
-    const index = this.$route.params.camIdx
-    return{
-      detailData : [],
-      index : index,
-      file : null,
-      fileList : [],
-      bucketName : 'wecode-msg',
-      butcketRegion : 'ap-northeast-2',
-      IdentityPoolId : 'ap-northeast-2:47ecc0a5-1a4d-40b0-85ce-8d80745c219a',
-      test : ''
-    }
+  data() {
+    const index = this.$route.params.camIdx;
+    return {
+      detailData: [],
+      index: index,
+      file: null,
+      fileList: [],
+    };
   },
-  methods : {
-    moveList(){
+  methods: {
+    moveList() {
       this.$router.push({
-          name : 'List'
-      })
+        name: "List",
+      });
     },
     openUrl(i) {
-      axios.get(`http://10.58.52.99:3001/scheduler/image?bucketPath=${i}`,{
-        headers : {
-          authorization : `Bearer ${localStorage.getItem('token')}`
-            }
-        }).then(res => {
-          this.test = 'data:image/png;base64,' + btoa(unescape(encodeURIComponent(res.data)))
+      axios
+        .get(`http://10.58.52.99:3001/scheduler/image?bucketPath=${i}`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         })
+        .then((res) => {
+          this.test =
+            "data:image/png;base64," +
+            btoa(unescape(encodeURIComponent(res.data)));
+        });
     },
-    changeTime(time,num){
+    changeTime(time, num) {
       const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
-      let koreaNow = new Date(new Date(time).getTime() + KR_TIME_DIFF).toISOString()
-      console.log(koreaNow)
-      if(num == 1){
-        let koreanTime = String(koreaNow).split('T')[num].split('.')[0]
-        return koreanTime
-      } else{
-        let koreanTime = String(koreaNow).split('T')[num]
-        return koreanTime
+      let koreaNow = new Date(
+        new Date(time).getTime() + KR_TIME_DIFF
+      ).toISOString();
+      console.log(koreaNow);
+      if (num == 1) {
+        let koreanTime = String(koreaNow).split("T")[num].split(".")[0];
+        return koreanTime;
+      } else {
+        let koreanTime = String(koreaNow).split("T")[num];
+        return koreanTime;
       }
-    }
-    
+    },
   },
   created() {
-    const index = this.$route.params.camIdx
-    axios.get(`http://3.36.103.222:80/scheduler?idx=${index}`,{
-            headers : {
-              authorization : `Bearer ${localStorage.getItem('token')}`
-            }
-          })
-    .then(res => {
-      console.log(res.data)
-      this.detailData = res.data
-    })
-  }
-}
+    const index = this.$route.params.camIdx;
+    axios
+      .get(`http://3.36.103.222:80/scheduler?idx=${index}`, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.detailData = res.data;
+      });
+  },
+};
 </script>
--- src/views/Register/Register.vue
+++ src/views/Register/Register.vue
@@ -3,179 +3,314 @@
     <h3 class="section-tit">캠페인 등록/수정</h3>
     <div class="form-wrap">
       <div class="form-box">
-        <input type="number" v-if="!($route.params.camIdx)" class="form-input" placeholder="캠페인번호" @input="inpNum" :value="inpData"/>
-        <input type="number" v-if="$route.params.camIdx" class="form-input" placeholder="캠페인번호" :value="idxData[0][0].campaign_idx" disabled/>
-        <button type="button" v-if="!($route.params.camIdx)" class="btn-blue-sm" @click="registerIdx">조회</button>
-        <button type="button" v-if="$route.params.camIdx" class="btn-blue-sm" disabled>조회</button>
+        <input
+          type="number"
+          v-if="!$route.params.camIdx"
+          class="form-input"
+          placeholder="캠페인번호"
+          @input="inpNum"
+          :value="inpData"
+        />
+        <input
+          type="number"
+          v-if="$route.params.camIdx"
+          class="form-input"
+          placeholder="캠페인번호"
+          :value="idxData[0][0].campaign_idx"
+          disabled
+        />
+        <button
+          type="button"
+          v-if="!$route.params.camIdx"
+          class="btn-blue-sm"
+          @click="registerIdx"
+        >
+          조회
+        </button>
+        <button
+          type="button"
+          v-if="$route.params.camIdx"
+          class="btn-blue-sm"
+          disabled
+        >
+          조회
+        </button>
       </div>
       <div class="flex-box">
         <div class="form-box">
           <label for="campaign-name" class="label">캠페인명</label>
-          <input type="text" v-if="$route.params.camIdx" id="campaign-name" class="form-input" :value="idxData[0][0].campaign_title" disabled>
-          <input type="text" v-if="!($route.params.camIdx)" id="campaign-name" class="form-input" :value="camTitle" disabled>
+          <input
+            type="text"
+            v-if="$route.params.camIdx"
+            id="campaign-name"
+            class="form-input"
+            :value="idxData[0][0].campaign_title"
+            disabled
+          />
+          <input
+            type="text"
+            v-if="!$route.params.camIdx"
+            id="campaign-name"
+            class="form-input"
+            :value="camTitle"
+            disabled
+          />
         </div>
         <div class="form-box">
           <label for="company-name" class="label">회사명</label>
-          <input type="text" v-if="$route.params.camIdx" id="company-name" class="form-input" :value="idxData[0][0].company_name" disabled>
-          <input type="text" v-if="!($route.params.camIdx)" id="company-name" class="form-input" :value="comName" disabled>
+          <input
+            type="text"
+            v-if="$route.params.camIdx"
+            id="company-name"
+            class="form-input"
+            :value="idxData[0][0].company_name"
+            disabled
+          />
+          <input
+            type="text"
+            v-if="!$route.params.camIdx"
+            id="company-name"
+            class="form-input"
+            :value="comName"
+            disabled
+          />
         </div>
       </div>
       <div class="flex-box">
         <div class="form-box">
           <p class="label">기간설정</p>
-          <input type="date" v-if="$route.params.camIdx" class="form-input" :value="idxData[0][0].start_day?.split('T')[0]" @input="startDate" data-placeholder="시작일" required/>
-          <input type="date" v-if="!($route.params.camIdx)" class="form-input" v-model="startDay" data-placeholder="시작일" required/>
+          <input
+            type="date"
+            v-if="$route.params.camIdx"
+            class="form-input"
+            :value="idxData[0][0].start_day?.split('T')[0]"
+            @input="startDate"
+            data-placeholder="시작일"
+            required
+          />
+          <input
+            type="date"
+            v-if="!$route.params.camIdx"
+            class="form-input"
+            v-model="startDay"
+            data-placeholder="시작일"
+            required
+          />
         </div>
         <div class="form-box">
-          <input type="date" v-if="$route.params.camIdx" class="form-input" :value="idxData[0][0].end_day?.split('T')[0]" @input="endDate" data-placeholder="종료일" required/>
-          <input type="date" v-if="!($route.params.camIdx)" class="form-input" v-model="endDay" data-placeholder="종료일" required/>
+          <input
+            type="date"
+            v-if="$route.params.camIdx"
+            class="form-input"
+            :value="idxData[0][0].end_day?.split('T')[0]"
+            @input="endDate"
+            data-placeholder="종료일"
+            required
+          />
+          <input
+            type="date"
+            v-if="!$route.params.camIdx"
+            class="form-input"
+            v-model="endDay"
+            data-placeholder="종료일"
+            required
+          />
         </div>
       </div>
       <div class="keyword-area">
-        <label for="keyword" class="label">키워드 <span class="sub-txt">* 한개씩 입력해주세요 (최대 3개)</span></label>
+        <label for="keyword" class="label"
+          >키워드
+          <span class="sub-txt">* 한개씩 입력해주세요 (최대 3개)</span></label
+        >
         <div class="form-box">
-          <input type="text" class="form-input" v-model="keytext" :disabled="(keyword.length >= 3) "/>
-          <button type="button" class="btn-gray-sm" @click="addKeyword" :disabled="keyword.length == 3">추가</button>
+          <input
+            type="text"
+            class="form-input"
+            v-model="keytext"
+            :disabled="keyword.length >= 3"
+          />
+          <button
+            type="button"
+            class="btn-gray-sm"
+            @click="addKeyword"
+            :disabled="keyword.length == 3"
+          >
+            추가
+          </button>
         </div>
         <div class="keyword">
-          <span v-for="(word,i) in keyword" :key="i">{{word}}<button type="button" class="btn-del" @click="delKeyword(i)">삭제</button></span>
+          <span v-for="(word, i) in keyword" :key="i"
+            >{{ word
+            }}<button type="button" class="btn-del" @click="delKeyword(i)">
+              삭제
+            </button></span
+          >
         </div>
       </div>
     </div>
     <div class="btn-area">
-      <button type="button" class="btn-gray" @click="$router.push({name:'List'})">취소</button>
-      <button type="button" v-if="!($route.params.camIdx)" class="btn-blue" @click="sendNewJob">등록</button>
-      <button type="button" v-else class="btn-blue" @click="sendModifyJob">수정</button>
+      <button
+        type="button"
+        class="btn-gray"
+        @click="$router.push({ name: 'List' })"
+      >
+        취소
+      </button>
+      <button
+        type="button"
+        v-if="!$route.params.camIdx"
+        class="btn-blue"
+        @click="sendNewJob"
+      >
+        등록
+      </button>
+      <button type="button" v-else class="btn-blue" @click="sendModifyJob">
+        수정
+      </button>
     </div>
-    <LoadingSpinner v-if="isLoading"/>
+    <LoadingSpinner v-if="isLoading" />
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import LoadingSpinner from '../../components/common/LoadingSpinner.vue'
+import axios from "axios";
+import LoadingSpinner from "../../components/common/LoadingSpinner.vue";
 export default {
   data() {
-    return{
-      idxData : [],
-      keytext : '',
-      keyword : [],
-      inpData : '',
-      isLoading : false,
-      start_day : '',
-      end_day : '',
-      camTitle : '',
-      comName : ''
-    }
+    return {
+      idxData: [],
+      keytext: "",
+      keyword: [],
+      inpData: "",
+      isLoading: false,
+      start_day: "",
+      end_day: "",
+      camTitle: "",
+      comName: "",
+    };
   },
-  components : {
-    LoadingSpinner : LoadingSpinner
+  components: {
+    LoadingSpinner: LoadingSpinner,
   },
-  methods : {
-    inpNum(event){
-      this.inpData = event.target.value
+  methods: {
+    inpNum(event) {
+      this.inpData = event.target.value;
     },
-    startDate(event){
-      this.start_day = event.target.value
+    startDate(event) {
+      this.start_day = event.target.value;
     },
-    endDate(event){
-      this.end_day = event.target.value
+    endDate(event) {
+      this.end_day = event.target.value;
     },
-    registerIdx(){
+    registerIdx() {
       this.isLoading = true;
-      const inpData = this.inpData
-        axios.get(`http://3.36.103.222:80/campaign/${inpData}`,{
-          headers :{
-            authorization : `Bearer ${localStorage.getItem('token')}`
-          }
+      const inpData = this.inpData;
+      axios
+        .get(`http://3.36.103.222:80/campaign/${inpData}`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         })
-        .then(res => {
-          console.log(res.data)
-          this.idxData.push(res.data)
+        .then((res) => {
+          console.log(res.data);
+          this.idxData.push(res.data);
           this.isLoading = false;
-          this.camTitle = this.idxData[0].title
-          this.comName = this.idxData[0].company
-        })
+          this.camTitle = this.idxData[0].title;
+          this.comName = this.idxData[0].company;
+        });
     },
-    addKeyword(){
-      if(this.keytext !== ""){
+    addKeyword() {
+      if (this.keytext !== "") {
         this.keyword.push(this.keytext);
-        this.keytext = ""
-      } else{
-        alert('키워드를 입력하세요')
+        this.keytext = "";
+      } else {
+        alert("키워드를 입력하세요");
       }
     },
-    delKeyword(i){
-      this.keyword.splice(i,1)
+    delKeyword(i) {
+      this.keyword.splice(i, 1);
     },
-    sendNewJob(){
-      console.log(this.startDay, this.endDay)
-      axios.post('http://3.36.103.222:80/scheduler',{
-          campaign_idx : this.inpData,
-          startDay : this.startDay,
-          endDay : this.endDay,
-          keyword : this.keyword.join()
-        },{
-        headers : {
-          authorization : `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-      .then(res => {
-        console.log(res)
-        if(res.data == 'post success!'){
-          this.$router.push({
-            name: 'List',
-          })  
-        } else{
-          throw new Error('에러 발생')
-        }
-      })
+    sendNewJob() {
+      console.log(this.startDay, this.endDay);
+      axios
+        .post(
+          "http://3.36.103.222:80/scheduler",
+          {
+            campaign_idx: this.inpData,
+            startDay: this.startDay,
+            endDay: this.endDay,
+            keyword: this.keyword.join(),
+          },
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          if (res.data == "post success!") {
+            this.$router.push({
+              name: "List",
+            });
+          } else {
+            throw new Error("에러 발생");
+          }
+        });
     },
-    sendModifyJob(){
-      console.log(this.start_day, this.end_day)
-      axios.patch(`http://3.36.103.222:80/scheduler/${this.idxData[0][0].idx}`,{
-          startDay : this.start_day,
-          endDay : this.end_day,
-          keyword : this.keyword.join()
-        },{
-        headers : {
-          authorization : `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-      .then(res => {
-        console.log(res)
-        if(res.data.message == 'patch success'){
-          this.$router.push({
-            name: 'List',
-          })  
-        } else{
-          throw new Error('에러 발생')
-        }
-      })
-    }
-
+    sendModifyJob() {
+      console.log(this.start_day, this.end_day);
+      axios
+        .patch(
+          `http://3.36.103.222:80/scheduler/${this.idxData[0][0].idx}`,
+          {
+            startDay: this.start_day,
+            endDay: this.end_day,
+            keyword: this.keyword.join(),
+          },
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          if (res.data.message == "patch success") {
+            this.$router.push({
+              name: "List",
+            });
+          } else {
+            throw new Error("에러 발생");
+          }
+        });
+    },
   },
-  created(){
-    if(this.$route.params.camIdx){
+  created() {
+    if (this.$route.params.camIdx) {
       this.isLoading = true;
-      const campaign_idx = this.$route.params.camIdx
-      axios.get(`http://3.36.103.222:80/scheduler?idx=${campaign_idx}`,{
-        headers : {
-          authorization : `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-      .then(res => {
-        console.log(res.data)
-        this.idxData.push(res.data)
-        this.keyword = this.idxData[0][0].keyword?.split(',')
-        this.isLoading = false;
-      })
+      const campaign_idx = this.$route.params.camIdx;
+      axios
+        .get(`http://3.36.103.222:80/scheduler?idx=${campaign_idx}`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.idxData.push(res.data);
+          this.keyword = this.idxData[0][0].keyword?.split(",");
+          this.isLoading = false;
+        });
     }
   },
-  watch:{
-		keytext : function(){
-			return this.keytext = this.keytext.replace(/[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\ '\"\\(\=]/gi, '');  
-		}
-	}
-}
+  watch: {
+    keytext: function () {
+      return (this.keytext = this.keytext.replace(
+        /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\ '\"\\(\=]/gi,
+        ""
+      ));
+    },
+  },
+};
 </script>
